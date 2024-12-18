1a2aa1b818e629be21ae2ed1cfd2d7648eca1d2a
Ilia Popov <iljapopov17@gmail.com>
2024-12-18 13:10:54 +0300

    refactor the R code

--- 02_Pangenome_vis_journal.R
+++ 02_Pangenome_vis_journal.R
@@ -9,7 +9,8 @@ setwd(main_dir)
 
 # Load/install required packages
 
-if (!require("pacman")) install.packages("pacman")
+if (!require("pacman"))
+  install.packages("pacman")
 
 pacman::p_load(ggplot2, dplyr, tidyr, ggnewscale)
 
@@ -23,8 +24,11 @@ pangenome_df <- read.csv("Pangenome/PanGenome-SaPh.All.prt-clust-0.8-mode1.lst.s
 # Add new columns to categorize genes
 pangenome_df_1 <- pangenome_df %>%
   mutate(
-    Core = ifelse(nb_members == 251, "Core", NA),  # Core genes (present in all genomes)
-    Shared = ifelse(nb_members > 1 & nb_members < 251, "Shared", NA),  # Shared genes (present in some genomes)
+    Core = ifelse(nb_members == 251, "Core", NA),
+    # Core genes (present in all genomes)
+    Shared = ifelse(nb_members > 1 &
+                      nb_members < 251, "Shared", NA),
+    # Shared genes (present in some genomes)
     Unique = ifelse(nb_members == 1, "Unique", NA)  # Unique genes (present in only one genome)
   )
 
@@ -41,28 +45,52 @@ gene_counts$fraction = gene_counts$n / sum(gene_counts$n)
 gene_counts$ymax = cumsum(gene_counts$fraction)
 
 # Compute the bottom of each rectangle
-gene_counts$ymin = c(0, head(gene_counts$ymax, n=-1))
+gene_counts$ymin = c(0, head(gene_counts$ymax, n = -1))
 
 # Compute label position
 gene_counts$labelPosition <- (gene_counts$ymax + gene_counts$ymin) / 2
 
 # Compute a good label
-gene_counts$label <- paste0(gene_counts$Type, "\n value: ", gene_counts$n, "\n", round((gene_counts$fraction * 100), 2), "%")
+gene_counts$label <- paste0(gene_counts$Type,
+                            "\n value: ",
+                            gene_counts$n,
+                            "\n",
+                            round((gene_counts$fraction * 100), 2),
+                            "%")
 
-colors <- c("Core" = "#d9e7f1", "Shared" = "#743f96", "Unique" = "#8dadd1")
+colors <- c(
+  "Core" = "#d9e7f1",
+  "Shared" = "#743f96",
+  "Unique" = "#8dadd1"
+)
 
 # Make the plot
-pangenome_donut <- ggplot(gene_counts, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Type)) +
+pangenome_donut <- ggplot(gene_counts,
+                          aes(
+                            ymax = ymax,
+                            ymin = ymin,
+                            xmax = 4,
+                            xmin = 3,
+                            fill = Type
+                          )) +
   geom_rect() +
-  geom_text( x=1.5, aes(y=labelPosition, label=label, color=Type), size=6) + # x here controls label position (inner / outer)
+  geom_text(x = 1.5,
+            aes(y = labelPosition, label = label, color = Type),
+            size = 6) + # x here controls label position (inner / outer)
   scale_fill_manual(values = colors) +
   scale_color_manual(values = colors) +
-  coord_polar(theta="y") +
+  coord_polar(theta = "y") +
   xlim(c(-1, 4)) +
   theme_void() +
   theme(legend.position = "none")
 
-ggsave("imgs/donut_chart.png", pangenome_donut, width = 8, height = 8, dpi = 600)
+ggsave(
+  "imgs/donut_chart.png",
+  pangenome_donut,
+  width = 8,
+  height = 8,
+  dpi = 600
+)
 
 ########################
 # Part 2: Scatter plot #
@@ -74,17 +102,34 @@ pangenome_scatter <- ggplot(pangenome_df, aes(x = num_fam, y = nb_members)) +
   geom_point(color = "grey", alpha = 0.5) +
   
   # Highlight gene family 414
-  geom_point(data = pangenome_df[pangenome_df$num_fam == 414, ], aes(x = num_fam, y = nb_members),
-             color = "green", alpha = 0.5, size = 3, shape = 23, 
-             stroke = 2) +
+  geom_point(
+    data = pangenome_df[pangenome_df$num_fam == 414, ],
+    aes(x = num_fam, y = nb_members),
+    color = "green",
+    alpha = 0.5,
+    size = 3,
+    shape = 23,
+    stroke = 2
+  ) +
   
   # Highlight gene family 415
-  geom_point(data = pangenome_df[pangenome_df$num_fam == 415, ], aes(x = num_fam, y = nb_members),
-             color = "red", alpha = 0.5, size = 1, shape = 21, 
-             stroke = 2) +
+  geom_point(
+    data = pangenome_df[pangenome_df$num_fam == 415, ],
+    aes(x = num_fam, y = nb_members),
+    color = "red",
+    alpha = 0.5,
+    size = 1,
+    shape = 21,
+    stroke = 2
+  ) +
   
   # Add a horizontal dashed line at y = 70
-  geom_hline(yintercept = 70, color = "black", linetype = "dashed", alpha = 0.5) +
+  geom_hline(
+    yintercept = 70,
+    color = "black",
+    linetype = "dashed",
+    alpha = 0.5
+  ) +
   
   # Customize axis labels and breaks
   scale_x_continuous(breaks = seq(0, 6000, by = 1000)) +  # Display every x-axis label
@@ -96,6 +141,12 @@ pangenome_scatter <- ggplot(pangenome_df, aes(x = num_fam, y = nb_members)) +
   labs(x = "Gene Family ID", y = "Number of Genomes Sharing the Gene") +
   
   # Ensure everything fits into the plot area
-  theme(plot.margin = unit(c(1,1,1,1), "cm"))
+  theme(plot.margin = unit(c(1, 1, 1, 1), "cm"))
 
-ggsave("imgs/scatter_plot.png", pangenome_scatter, width = 10, height = 6, dpi = 600)
+ggsave(
+  "imgs/scatter_plot.png",
+  pangenome_scatter,
+  width = 10,
+  height = 6,
+  dpi = 600
+)
--- 03_phylo_journal.R
+++ 03_phylo_journal.R
@@ -7,17 +7,26 @@
 main_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(main_dir)
 
-if (!require("pacman")) install.packages("pacman")
+if (!require("pacman"))
+  install.packages("pacman")
 
-pacman::p_load(ggtree, ggimage, phangorn, ggplot2, treeio, ggnewscale, viridis, phytools, patchwork)
+pacman::p_load(ggtree,
+               ggimage,
+               phangorn,
+               ggplot2,
+               treeio,
+               ggnewscale,
+               viridis,
+               phytools,
+               patchwork)
 
 ############################
 # Part 1: Metadata parsing #
 ############################
 
-metadata <- read.table('metadata/metadata.tsv', sep='\t', header=T)
+metadata <- read.table('metadata/metadata.tsv', sep = '\t', header = T)
 
-meta.year <- as.data.frame(metadata[,'Year'])
+meta.year <- as.data.frame(metadata[, 'Year'])
 colnames(meta.year) <- 'Year'
 rownames(meta.year) <- metadata$Name
 meta.year$Year[meta.year$Year == "ND"] <- NA
@@ -54,9 +63,14 @@ country_code_map <- c(
 metadata$flag_code <- country_code_map[metadata$Country]
 metadata$flag_code[is.na(metadata$flag_code)] <- NA
 
-if (!dir.exists("flags")) dir.create("flags")
+if (!dir.exists("flags"))
+  dir.create("flags")
 
-flag_urls <- paste0("https://raw.githubusercontent.com/HatScripts/circle-flags/gh-pages/flags/", unique(metadata$flag_code[!is.na(metadata$flag_code)]), ".svg")
+flag_urls <- paste0(
+  "https://raw.githubusercontent.com/HatScripts/circle-flags/gh-pages/flags/",
+  unique(metadata$flag_code[!is.na(metadata$flag_code)]),
+  ".svg"
+)
 
 for (i in seq_along(flag_urls)) {
   country_code <- sub(".*/flags/(.*)\\.svg", "\\1", flag_urls[i])
@@ -77,34 +91,66 @@ SaPh_tree <- read.tree("phylogenetics/tree/tree_ufb.treefile")
 
 #midpoint.root(SaPh_tree)
 
-SaPh <- ggtree(SaPh_tree) %<+% metadata + 
-  xlim(0,0.8) + 
-  geom_tiplab(
-    aes(label=Full.Name),
-    color="black",
-    align=TRUE
-  )
+SaPh <- ggtree(SaPh_tree) %<+% metadata +
+  xlim(0, 0.8) +
+  geom_tiplab(aes(label = Full.Name),
+              color = "black",
+              align = TRUE)
 
 SaPh_boot <- SaPh$data
-SaPh_boot <- SaPh_boot[!SaPh_boot$isTip,]
+SaPh_boot <- SaPh_boot[!SaPh_boot$isTip, ]
 SaPh_boot$label <- as.numeric(SaPh_boot$label)
 SaPh_boot$bootstrap <- '0'
 SaPh_boot$bootstrap[SaPh_boot$label >= 70] <- '1'
 SaPh_boot$bootstrap[is.na(SaPh_boot$label)] <- '1'
 
 SaPh <- SaPh + new_scale_color() +
-  geom_tree(data=SaPh_boot, aes(color=bootstrap == '1')) +
-  scale_color_manual(name='Bootstrap', values=setNames(c("black", "grey"), c(T,F)), guide = "none")
+  geom_tree(data = SaPh_boot, aes(color = bootstrap == '1')) +
+  scale_color_manual(name = 'Bootstrap',
+                     values = setNames(c("black", "grey"), c(T, F)),
+                     guide = "none")
 
-SaPh <- SaPh + geom_tiplab(aes(image = flag_path), geom = "image", offset = 0.28, align = TRUE, size = 0.01, linesize = 0)
+SaPh <- SaPh + geom_tiplab(
+  aes(image = flag_path),
+  geom = "image",
+  offset = 0.28,
+  align = TRUE,
+  size = 0.01,
+  linesize = 0
+)
 
-SaPh <- gheatmap(SaPh, meta.year, width=0.05, offset=0.3, color="black", font.size=4, colnames_offset_y = -0.05) + 
-  scale_fill_viridis(option="D", name="Year", discrete = TRUE, na.translate = T)
+SaPh <- gheatmap(
+  SaPh,
+  meta.year,
+  width = 0.05,
+  offset = 0.3,
+  color = "black",
+  font.size = 4,
+  colnames_offset_y = -0.05
+) +
+  scale_fill_viridis(
+    option = "D",
+    name = "Year",
+    discrete = TRUE,
+    na.translate = T
+  )
 
 SaPh <- SaPh +
-  annotate("text", x=max(SaPh$data$x) + 0.28, y=-0.05, label="Country", size=4)
+  annotate(
+    "text",
+    x = max(SaPh$data$x) + 0.28,
+    y = -0.05,
+    label = "Country",
+    size = 4
+  )
 
-ggsave('imgs/SaPh_tree.png', SaPh, width=14, height=16, dpi=600)
+ggsave(
+  'imgs/SaPh_tree.png',
+  SaPh,
+  width = 14,
+  height = 16,
+  dpi = 600
+)
 
 #####################
 # Part 3: SaPh MSA #
@@ -116,9 +162,23 @@ library(ggplotify)
 
 protein_sequences <- 'Alignment/Align-SaPh/renamed_SaPh-mafft-align.414.aln'
 
-msa <- ggmsa(protein_sequences, 1, 141, char_width = 0.5, seq_name = TRUE,
-      consensus_views = TRUE, disagreement = FALSE, use_dot = FALSE)
+msa <- ggmsa(
+  protein_sequences,
+  1,
+  141,
+  char_width = 0.5,
+  seq_name = TRUE,
+  consensus_views = TRUE,
+  disagreement = FALSE,
+  use_dot = FALSE
+)
 
 saph_msa <- as.grob(msa)
 
-ggsave("imgs/SaPh_MSA.png", plot = saph_msa, width = 25, height = 12, dpi = 600)
+ggsave(
+  "imgs/SaPh_MSA.png",
+  plot = saph_msa,
+  width = 25,
+  height = 12,
+  dpi = 600
+)
